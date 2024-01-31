1516d67c6de88d39a6491aa527acce9d9e0a9ae2
OngGuny <start1step@naver.com>
2024-02-01 00:45:17 +0900

    learn module.css, named-export, export default, react function docs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import { useEffect, useState }  from "react";
+import {Button} from './Button';
 function App() {
+// how to prevent rerender when state change? useStatu is impossible to do that. Because useState rerender( = run every codes in component.js) component when state change. So What?
+// -> i want render component only once.
+  const [counter,setCounter] = useState(0);
+
+  const onlyOnce = () => {
+    console.log('redner once');
+  }
+  useEffect( () => {
+    console.log('redner once');
+  },[]);
+
+  const onClick = () => setCounter((prev) => prev +1);
+  console.log('render when clicked');
   return (
 <div>
-  <h1>Welcome React!</h1>
+  <Button text={'continue'}/>
+  <h1>{counter}</h1>
+  <button onClick={onClick}>Click Me</button>
 </div>
   );
 }
