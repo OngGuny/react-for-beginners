629cf89a265099ced457ce522ea2d343b121bf3f
OngGuny <start1step@naver.com>
2024-02-02 01:08:35 +0900

    learn how to useEffect and for what, to prevent rerendering by change from status, remove strictmode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import { useEffect, useState }  from "react";
-import {Button} from './Button';
+
 function App() {
 // how to prevent rerender when state change? useStatu is impossible to do that. Because useState rerender( = run every codes in component.js) component when state change. So What?
 // -> i want render component only once.
   const [counter,setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
 
-  const onlyOnce = () => {
-    console.log('redner once');
-  }
+  const onChange = (event) =>
+    setKeyword(event.target.value);
+    console.log('redner once when rerender.');
+ useEffect(() => {
+    console.log('redner once when counter changed.');
+  },[counter]);
   useEffect( () => {
-    console.log('redner once');
+    console.log('redner once, cause empty array = no dependancy... read docs..., watch nothing');
   },[]);
+  useEffect(()=>{
+    if(keyword !== "" && keyword.length > 5){
+      console.log('log when keyword change'+ keyword);
 
+    }
+  },[keyword])
   const onClick = () => setCounter((prev) => prev +1);
-  console.log('render when clicked');
+
+  console.log('onRerender , i\'m in component' );
   return (
 <div>
-  <Button text={'continue'}/>
+  <input
+  value={keyword}
+  onChange={onChange}
+  type="text"
+  placeholder="Search here.."
+  />
   <h1>{counter}</h1>
   <button onClick={onClick}>Click Me</button>
 </div>
